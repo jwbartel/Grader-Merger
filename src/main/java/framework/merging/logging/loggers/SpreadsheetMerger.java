28b41a46bb865925d74c1626cf5299d2524b81fd
Jacob Bartel <bartel@cs.unc.edu>
2014-01-27 12:12:39 -0500

    Refactored code to multiple methods for easier understanding

--- src/main/java/framework/merging/logging/loggers/SpreadsheetMerger.java
+++ src/main/java/framework/merging/logging/loggers/SpreadsheetMerger.java
@@ -21,6 +21,10 @@ import framework.merging.rules.ResultTypes;
 
 public class SpreadsheetMerger extends MergerLogger {
 
+	private Set<String> mergedOnyens = new HashSet<String>();
+	private Set<String> unmergedOnyens = new HashSet<String>();
+	private ArrayList<String> featuresAndRestrictions = new ArrayList<String>();
+
 	/*
 	 * Writes the header from input into the output workbook
 	 * 
@@ -49,16 +53,75 @@ public class SpreadsheetMerger extends MergerLogger {
 		return featuresAndRestrictions;
 	}
 
-	private void mergeRows(XSSFWorkbook outputWorkbook, XSSFWorkbook resultWorkbook,
-			XSSFWorkbook mergedWorkbook, ArrayList<String> featuresAndRestrictions) {
+	/*
+	 * Export user id data to output row that does not change with merging
+	 */
+	private void outputUserIdData(XSSFRow outputRow, XSSFRow inputRow) {
+		outputRow.createCell(0).setCellValue(inputRow.getCell(0).getStringCellValue());
+		outputRow.createCell(1).setCellValue(inputRow.getCell(1).getStringCellValue());
+		outputRow.createCell(2).setCellValue(inputRow.getCell(2).getStringCellValue());
+		outputRow.createCell(3).setCellValue(inputRow.getCell(3).getStringCellValue());
+	}
 
-		Set<String> mergedOnyens = new HashSet<String>();
-		Set<String> resultOnyens = new HashSet<String>();
+	/*
+	 * Used for the case when score data has nothing to merge with
+	 */
+	private void outputScoreData(XSSFRow outputRow, XSSFRow inputRow,
+			ArrayList<String> featuresAndRestrictions, int rowNum) {
+		outputRow.createCell(4).setCellValue(inputRow.getCell(4).getNumericCellValue());
+		outputRow.createCell(5).setCellValue(inputRow.getCell(5).getNumericCellValue());
+		outputRow.createCell(6).setCellFormula("E" + (rowNum + 1) + "*F" + (rowNum + 1));
+		for (int i = 0; i < featuresAndRestrictions.size(); i++) {
+			outputRow.createCell(i + 7).setCellValue(inputRow.getCell(i + 7).getNumericCellValue());
+		}
+	}
 
-		XSSFSheet outputSheet = outputWorkbook.getSheetAt(0);
-		XSSFSheet resultSheet = resultWorkbook.getSheetAt(0);
-		XSSFSheet mergedSheet = mergedWorkbook.getSheetAt(0);
+	private void mergeScoreData(XSSFRow outputRow, XSSFRow inputRow1, XSSFRow inputRow2,
+			ArrayList<String> featuresAndRestrictions, int rowNum) {
 
+		// Output the raw score
+		Double rawVal1 = inputRow1.getCell(4).getNumericCellValue();
+		Double rawVal2 = inputRow2.getCell(4).getNumericCellValue();
+		Double outputRaw = (Double) MergingEnvironment.get().getMergingRules()
+				.getMergedVal(ResultTypes.RAW_SCORE, rawVal1, rawVal2);
+		outputRow.createCell(4).setCellValue(outputRaw);
+
+		// Output the late or early penalty
+		Double penaltyVal1 = inputRow1.getCell(5).getNumericCellValue();
+		Double penaltyVal2 = inputRow2.getCell(5).getNumericCellValue();
+		Double outputPenalty = (Double) MergingEnvironment.get().getMergingRules()
+				.getMergedVal(ResultTypes.LATE_OR_EARLY_PENALTY, penaltyVal1, penaltyVal2);
+		outputRow.createCell(5).setCellValue(outputPenalty);
+
+		// Output the formulat for final score
+		outputRow.createCell(6).setCellFormula("E" + (rowNum + 1) + "*F" + (rowNum + 1));
+
+		// Output all features and restrictions
+		for (int i = 0; i < featuresAndRestrictions.size(); i++) {
+
+			String featureOrRestrictionName = featuresAndRestrictions.get(i);
+			Double val1 = inputRow1.getCell(i + 7).getNumericCellValue();
+			Double val2 = inputRow2.getCell(i + 7).getNumericCellValue();
+			Double outputVal = (Double) MergingEnvironment
+					.get()
+					.getMergingRules()
+					.getMergedVal(new ResultTypes.NamedResultType(featureOrRestrictionName), val1,
+							val2);
+			outputRow.createCell(i + 7).setCellValue(outputVal);
+		}
+	}
+
+	/*
+	 * Export all rows that already exist in the previously merged sheet
+	 * (mergedSheet) to the output sheet (outputSheet)
+	 * 
+	 * If these rows also exist in the non-merged sheet (resultSheet) merge
+	 * those into the output as well
+	 * 
+	 * returns the next row number in the output sheet
+	 */
+	private int outputAlreadyMergedRows(XSSFSheet outputSheet, XSSFSheet mergedSheet,
+			XSSFSheet unmergedSheet, ArrayList<String> featuresAndRestrictions) {
 		int rowNum;
 		for (rowNum = 1; rowNum <= mergedSheet.getLastRowNum(); rowNum++) {
 
@@ -66,83 +129,38 @@ public class SpreadsheetMerger extends MergerLogger {
 
 			// Write user data that does not change with merge
 			XSSFRow mergedRow = mergedSheet.getRow(rowNum);
-			outputRow.createCell(0).setCellValue(mergedRow.getCell(0).getStringCellValue());
-			outputRow.createCell(1).setCellValue(mergedRow.getCell(1).getStringCellValue());
-			outputRow.createCell(2).setCellValue(mergedRow.getCell(2).getStringCellValue());
-			outputRow.createCell(3).setCellValue(mergedRow.getCell(3).getStringCellValue());
+			outputUserIdData(outputRow, mergedRow);
 
 			String onyen = mergedRow.getCell(0).getStringCellValue().toLowerCase();
 			if (onyen != null && onyen.length() > 0) {
-
-				if (onyen.equals("amyzhang") || onyen.equals("lindzh")) {
-					System.out.println("reached");
-				}
 
 				// Mark this onyen as seen before
 				mergedOnyens.add(onyen);
 
 				// Find the row from non-merged results with this same onyen
-				XSSFRow resultRow = findRow(onyen, resultSheet, resultOnyens);
-				if (resultRow != null) {
-					// Merge the non-merged results with already merged one
-
-					// Output the raw score
-					Double mergedRaw = mergedRow.getCell(4).getNumericCellValue();
-					Double resultRaw = resultRow.getCell(4).getNumericCellValue();
-					Double outputRaw = (Double) MergingEnvironment.get().getMergingRules()
-							.getMergedVal(ResultTypes.RAW_SCORE, mergedRaw, resultRaw);
-					outputRow.createCell(4).setCellValue(outputRaw);
-
-					// Output the late or early penalty
-					Double mergedLate = mergedRow.getCell(5).getNumericCellValue();
-					Double resultLate = resultRow.getCell(5).getNumericCellValue();
-					Double outputLate = (Double) MergingEnvironment
-							.get()
-							.getMergingRules()
-							.getMergedVal(ResultTypes.LATE_OR_EARLY_PENALTY, mergedLate, resultLate);
-					outputRow.createCell(5).setCellValue(outputLate);
-
-					// Output the formulat for final score
-					outputRow.createCell(6)
-							.setCellFormula("E" + (rowNum + 1) + "*F" + (rowNum + 1));
-
-					// Output all features and restrictions
-					for (int i = 0; i < featuresAndRestrictions.size(); i++) {
-
-						String featureOrRestrictionName = featuresAndRestrictions.get(i);
-						Double mergedVal = mergedRow.getCell(i + 7).getNumericCellValue();
-						Double resultVal = resultRow.getCell(i + 7).getNumericCellValue();
-						Double outputVal = (Double) MergingEnvironment
-								.get()
-								.getMergingRules()
-								.getMergedVal(
-										new ResultTypes.NamedResultType(featureOrRestrictionName),
-										mergedVal, resultVal);
-						outputRow.createCell(i + 7).setCellValue(outputVal);
-					}
+				XSSFRow nonMergedRow = findRow(onyen, unmergedSheet, unmergedOnyens);
+				if (nonMergedRow != null) {
+					mergeScoreData(outputRow, mergedRow, nonMergedRow, featuresAndRestrictions,
+							rowNum);
 
 				} else {
 					// No row from non-merged results, so we only write already
 					// merged results to the output
-					outputRow.createCell(4)
-							.setCellValue(mergedRow.getCell(4).getNumericCellValue());
-					outputRow.createCell(5)
-							.setCellValue(mergedRow.getCell(5).getNumericCellValue());
-					outputRow.createCell(6)
-							.setCellFormula("E" + (rowNum + 1) + "*F" + (rowNum + 1));
-					for (int i = 0; i < featuresAndRestrictions.size(); i++) {
-						outputRow.createCell(i + 7).setCellValue(
-								mergedRow.getCell(i + 7).getNumericCellValue());
-					}
+					outputScoreData(outputRow, mergedRow, featuresAndRestrictions, rowNum);
 				}
 			}
 		}
+		return rowNum;
+	}
 
-		// Output any rows from the non-merged sheet that have not already been
-		// merged
-		resultOnyens.removeAll(mergedOnyens);
-		for (String onyen : resultOnyens) {
-			XSSFRow resultRow = findRow(onyen, resultSheet, new HashSet<String>());
+	/*
+	 * Export to output all rows that were in the unmerged sheet but not in the
+	 * merged sheet
+	 */
+	private void outputMissingNonMergedRows(XSSFSheet outputSheet, XSSFSheet unmergedSheet,
+			int rowNum) {
+		for (String onyen : unmergedOnyens) {
+			XSSFRow resultRow = findRow(onyen, unmergedSheet, new HashSet<String>());
 			if (resultRow != null) {
 				XSSFRow outputRow = outputSheet.createRow(rowNum);
 				outputRow.createCell(4).setCellValue(resultRow.getCell(4).getNumericCellValue());
@@ -155,6 +173,29 @@ public class SpreadsheetMerger extends MergerLogger {
 				rowNum++;
 			}
 		}
+	}
+
+	/*
+	 * Merge all rows from the previously merged workbook and the unmerged
+	 * workbook
+	 */
+	private void mergeRows(XSSFWorkbook outputWorkbook, XSSFWorkbook unmergedWorkbook,
+			XSSFWorkbook mergedWorkbook) {
+
+		mergedOnyens = new HashSet<String>();
+		unmergedOnyens = new HashSet<String>();
+
+		XSSFSheet outputSheet = outputWorkbook.getSheetAt(0);
+		XSSFSheet unmergedSheet = unmergedWorkbook.getSheetAt(0);
+		XSSFSheet mergedSheet = mergedWorkbook.getSheetAt(0);
+
+		int rowNum = outputAlreadyMergedRows(outputSheet, mergedSheet, unmergedSheet,
+				featuresAndRestrictions);
+
+		// Output any rows from the non-merged sheet that have not already been
+		// merged
+		unmergedOnyens.removeAll(mergedOnyens);
+		outputMissingNonMergedRows(outputSheet, unmergedSheet, rowNum);
 	}
 
 	private XSSFRow findRow(String onyen, XSSFSheet sheet, Set<String> foundOnyens) {
@@ -179,9 +220,9 @@ public class SpreadsheetMerger extends MergerLogger {
 		XSSFWorkbook resultWorkbook = new XSSFWorkbook(new FileInputStream(spreadsheetResults));
 		XSSFWorkbook mergedWorkbook = new XSSFWorkbook(new FileInputStream(mergedSpreadsheet));
 
-		ArrayList<String> featuresAndRestrictions = writeHeaders(outputWorkbook, mergedWorkbook);
+		featuresAndRestrictions = writeHeaders(outputWorkbook, mergedWorkbook);
 
-		mergeRows(outputWorkbook, resultWorkbook, mergedWorkbook, featuresAndRestrictions);
+		mergeRows(outputWorkbook, resultWorkbook, mergedWorkbook);
 
 		outputWorkbook.write(new FileOutputStream(mergedSpreadsheet));
 		System.out.println("\tdone.");
